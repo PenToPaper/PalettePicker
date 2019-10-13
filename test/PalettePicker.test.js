0258bb2a648e27c8da2aea7199f7ff0d8ab59275
PenToPaper <wheelm11@gmail.com>
2019-10-12 20:21:56 -0400

    Added enzyme to jest tests

--- test/PalettePicker.test.js
+++ test/PalettePicker.test.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "../src/App";
+import { shallow } from "enzyme";
 
 it("renders without crashing", () => {
     expect(1).toEqual(1);
     const div = document.createElement("div");
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
+
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("header")).toHaveLength(1);
 });
