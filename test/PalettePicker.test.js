1b3870823dffbd36c8002b60b472a0d4cc002f69
PenToPaper <wheelm11@gmail.com>
2019-10-12 20:13:23 -0400

    Refactored test workflow

---
+++ test/PalettePicker.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../src/App";
+
+it("renders without crashing", () => {
+    expect(1).toEqual(1);
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
